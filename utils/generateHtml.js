a8506bdc00f7c79920af0b107611cf29e93fb1c8
Samuel <samuel@Samuels-MacBook-Pro.local>
2021-02-14 20:49:43 -0800

    html is now generated based on input

--- utils/generateHtml.js
+++ utils/generateHtml.js
@@ -1,7 +1,21 @@
+//creates cards for each employee
+function cards (data){
+    
+}
+
+
+
+
+
+
+
+
+
+
 function generateHtml(data){
 return `
 <!DOCTYPE html>
-<html lang="en">
+<html class="bg-secondary" lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,11 +30,22 @@ return `
     href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap"
     rel="stylesheet"
   />
+  <link
+    rel="stylesheet"
+    href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+  />
 </head>
 <body>
+<header class = "bg-dark text-light" >
+<h1>Employee Lookup
+<i class="fas fa-users"></i>
+</h1>
 
+</header>
+
+<div  id="container" class=" bg-secondary"> 
 <h1>This is a ${data[0].name} </h1>
-<p>This is a paragraph.</p>
+</div>
 
 <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 <script src="./index.js"></script>
